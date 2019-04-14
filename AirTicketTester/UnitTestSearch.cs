f46a6ba5358f5ea4f247f5c5804b133d526f71d6
nguyenquangdunginfo <nguyenquangdunginfo@gmail.com>
2019-04-14 19:08:32 +0700

    Test SearchThongTinKhachHang

---
+++ AirTicketTester/UnitTestSearch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+using AirTicketBookingManagement;
+namespace AirTicketTester
+{
+    [TestClass]
+    public class UnitTestSearch
+    {
+        [TestMethod]
+        public void TestSearchCMNDKH()
+        {
+            Connection conn = new Connection();
+            DataTable dulieu = conn.ExecuteData("select * from ChiTietDatVe where CMND ='241154073' ");
+            Assert.IsTrue(dulieu.Rows.Count > 0);
+        }
+    }
+}

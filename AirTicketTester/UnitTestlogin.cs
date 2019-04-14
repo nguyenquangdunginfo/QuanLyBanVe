923d8e3d99014537d396acfc566f154de1718e89
nguyenquangdunginfo <nguyenquangdunginfo@gmail.com>
2019-04-14 13:31:46 +0700

    Test Login001

---
+++ AirTicketTester/UnitTestlogin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+using AirTicketBookingManagement;
+
+
+namespace AirTicketTester
+{
+    [TestClass]
+    public class UnitTestlogin
+    {
+        [TestMethod]
+        public void TestMethodLogin001()
+        {
+            Connection conn = new Connection();
+            DataTable dulieu = conn.ExecuteData("select * from Account where Username ='admin' and Password='123'");
+            Assert.IsTrue(dulieu.Rows.Count > 0);
+        }
+        
+    }
+}

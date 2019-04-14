21657f3f26e707e59a9f774f606860dd0a415008
nguyenquangdunginfo <nguyenquangdunginfo@gmail.com>
2019-04-14 13:41:51 +0700

    Test Login002

--- AirTicketTester/UnitTestlogin.cs
+++ AirTicketTester/UnitTestlogin.cs
@@ -19,6 +19,16 @@ namespace AirTicketTester
             DataTable dulieu = conn.ExecuteData("select * from Account where Username ='admin' and Password='123'");
             Assert.IsTrue(dulieu.Rows.Count > 0);
         }
-        
+
+        [TestMethod]
+        public void Test_pass_notEmpty()
+        {
+            Connection conn = new Connection();
+            DataTable dulieu_tong = conn.ExecuteData("select * from Account");
+            int count_dulieu_tong = dulieu_tong.Rows.Count;
+            DataTable dulieu = conn.ExecuteData("select * from Account where Password IS NOT NULL");
+            Assert.IsTrue(dulieu.Rows.Count == count_dulieu_tong);
+        }
+
     }
 }

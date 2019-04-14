10c2b221ccda811658eb3f4f13fc5a25c18ae68f
nguyenquangdunginfo <nguyenquangdunginfo@gmail.com>
2019-04-14 21:13:13 +0700

    Test CMND_ISNUMERIC_SUM

--- AirTicketTester/UnitTestChangePass.cs
+++ AirTicketTester/UnitTestChangePass.cs
@@ -11,10 +11,7 @@ namespace AirTicketTester
         [TestMethod]
         public void Test_ChangePass_Success()
         {
-            Connection conn = new Connection();
-            DataTable dulieu = conn.ExecuteData("select * from Account where Username ='" );
            
-            Assert.IsTrue(dulieu.Rows.Count > 0);
         }
     }
 }
--- AirTicketTester/UnitTestFormDatVe.cs
+++ AirTicketTester/UnitTestFormDatVe.cs
@@ -14,5 +14,16 @@ namespace AirTicketTester
             DataTable dulieu = conn.ExecuteData("select * from ChiTietDatVe where CMND ='241154073' ");
             Assert.IsTrue(dulieu.Rows.Count > 0);
         }
+
+        [TestMethod]
+        public void Test_CMND_ISNUMERIC_SUM()
+        {
+            Connection conn = new Connection();
+            string mess = "Kiem tra Tong So CMND hop le la so nguyen";
+            DataTable dulieu_tong = conn.ExecuteData("select * from ChiTietDatVe");
+            int count_dulieu_tong = dulieu_tong.Rows.Count;
+            DataTable dulieu = conn.ExecuteData("select * from ChiTietDatVe where ISNUMERIC(CMND) > 0 ");
+            Assert.IsTrue(dulieu.Rows.Count == count_dulieu_tong, mess);
+        }
     }
 }
